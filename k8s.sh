612bbf532407a8d66416ed8ef515bf5ad3190d4c
rabin prj <rabin.kod123@gmail.com>
2020-06-13 17:30:16 +0545

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,4 +1,4 @@
-aws eks --region us-east-1 update-kubeconfig --name capstone/udacityprj5:v1.0
+aws eks --region us-east-1 update-kubeconfig --name capstone/udacityprj5
 kubectl apply -f  app-deployment.yaml
 kubectl get nodes
 kubectl get pods
