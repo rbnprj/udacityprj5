86dc901e82b0262da8cad6c78f0c7fd0ab44f461
EC2 Default User <ec2-user@ip-172-31-53-36.ec2.internal>
2020-06-13 08:10:19 +0000

    update

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,7 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=rabinprj123/capstone/udacityprj5v1.0
+dockerpath=rabinprj123/udacityprj5
 DOCKER_USERNAME=''
  DOCKER_PASSWORD=''
 # Step 2:  
@@ -14,7 +14,7 @@ DOCKER_USERNAME=''
 echo "Docker ID and Image: $dockerpath"
 #docker login
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker tag capstone/udacityprj5:v1.0 $dockerpath 
+docker tag udacityprj5 $dockerpath 
 
 # Step 3:
 # Push image to a docker repository
